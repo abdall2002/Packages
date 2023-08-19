fac6949ae1db9ca6a90176d713bcaf12df24b9e0
unknown <abdallahaliahmed517@gmail.com>
2023-08-19 11:53:52 +0300

    aaa

--- CAFacebook/Program.cs
+++ CAFacebook/Program.cs
@@ -4,39 +4,44 @@ using System;
 using CAFacebook;
 
 var comments = new List<FBComment>
-            {
-                new FBComment
-                {
-                    Owner = "Issam A.",
-                    Comment = "I Think ASP NET Core is the most powerfull Web Framework",
-                    CreatedAt = new DateTime (2021, 01, 01, 10, 30, 00)
-                },
-                new FBComment {
-                    Owner = "Reem S.",
-                    Comment = "Personally I Prefere Using C# with it.",
-                    CreatedAt = new DateTime(2021, 02, 01, 10, 30, 00)
-                },
-                new FBComment {
-                    Owner = "Issam A.",
-                    Comment = "Have you Tried Using Blazor?",
-                    CreatedAt = new DateTime(2021, 06, 01, 10, 30, 00)
-                },
-                new FBComment {
-                    Owner = "Reem S.",
-                    Comment = "Have you Tried Using Blazor?",
-                    CreatedAt = new DateTime(2021, 06, 28, 10, 30, 00)
-                },
-                new FBComment {
-                    Owner = "Abeer B.",
-                    Comment = "I Prefer VB over C#.",
-                    CreatedAt = new DateTime(2021, 07, 11, 10, 30, 00)
-                },
-                new FBComment {
-                    Owner = "Abeer B.",
-                    Comment = "VB is not from C family languages, It's hard for me",
-                    CreatedAt = DateTime.Now.AddMinutes(-5)
-                }
-            };
+{
+    new FBComment
+    {
+        Owner = "Issam A.",
+        Comment = "I Think ASP NET Core is the most powerfull Web Framework",
+        CreatedAt = new DateTime (2021, 01, 01, 10, 30, 00)
+    },
+    new FBComment 
+    {
+        Owner = "Reem S.",
+        Comment = "Personally I Prefere Using C# with it.",
+        CreatedAt = new DateTime(2021, 02, 01, 10, 30, 00)
+    },
+    new FBComment
+    {
+        Owner = "Issam A.",
+        Comment = "Have you Tried Using Blazor?",
+        CreatedAt = new DateTime(2021, 06, 01, 10, 30, 00)
+    },
+    new FBComment
+    {
+        Owner = "Reem S.",
+        Comment = "Have you Tried Using Blazor?",
+        CreatedAt = new DateTime(2021, 06, 28, 10, 30, 00)
+    },
+    new FBComment
+    {
+        Owner = "Abeer B.",
+        Comment = "I Prefer VB over C#.",
+        CreatedAt = new DateTime(2021, 07, 11, 10, 30, 00)
+    },
+    new FBComment
+    {
+        Owner = "Abeer B.",
+        Comment = "VB is not from C family languages, It's hard for me",
+        CreatedAt = DateTime.Now.AddMinutes(-5)
+    }
+};
 
 
 
